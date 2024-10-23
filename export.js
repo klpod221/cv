cf32a80e22c32bc74db1bfc45ee66d18e05f1cd9
klpod221 <klpod221@gmail.com>
2024-10-23 23:42:23 +0700

    chore: Update export script and add download button to CV

--- export.js
+++ export.js
@@ -3,28 +3,28 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
   const page = await browser.newPage();
 
-  // serve the index.html file and export to pdf
-  await page.goto('file://' + __dirname + '/index.html', {
-    waitUntil: 'networkidle0',
+  await page.goto("file://" + __dirname + "/index.html", {
+    waitUntil: "networkidle0",
   });
 
-  console.log('Exporting to PDF...');
+  await page.evaluate(() => {
+    const downloadBtn = document.querySelector(".download-btn");
+    downloadBtn.remove();
+  });
 
-  // export to pdf
   await page.pdf({
-    path: 'output/cv.pdf',
-    format: 'A4'
+    path: "output/cv.pdf",
+    format: "A4",
   });
 
-  // export to png
   await page.screenshot({
-    path: 'output/cv.png',
-    fullPage: true
+    path: "output/cv.png",
+    fullPage: true,
   });
 
   await browser.close();
